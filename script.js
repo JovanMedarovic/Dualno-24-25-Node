a3390336c7b734b100ebf77aa4d9ab471c165f3f
Jovan Medarovic <medarovicjovan@gmail.com>
2025-11-28 10:24:22 +0100

    #2

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+const listElement = document.getElementById("listToShow");
+
+const inputElement = document.getElementById("inputPostName");
+const addBtn = document.getElementById("addPostBtn");
+
 // Prikupljanje podataka preko "fetch" funkcije, uz pomoc GET metode
 const getData = async () => {
   const response = await fetch("http://localhost:3000/posts");
   const posts = await response.json();
-  console.log(posts);
-}
-
-// getData();
+  posts.map((post) => {
+    const deleteBtnElement = document.createElement("button");
+    deleteBtnElement.textContent = "X";
+    const pElement = document.createElement("p");
+    pElement.textContent=`${post.title} ------ views: ${post.views}`;
+    const liElement = document.createElement("li");
+    liElement.appendChild(pElement);
+    liElement.appendChild(deleteBtnElement);
+    listElement.appendChild(liElement);
+  });
+};
+getData();
 
 // postovanje podataka preko "fetch" funkcije, uz pomoc POST metode
-const post3 = { id: "3", title: "another title 3", views: 300 }; // mockovan payload
+// const post3 = { id: "3", title: "another title 3", views: 300 }; // mockovan payload
 
 const postData = async (payload) => {
   const response = await fetch("http://localhost:3000/posts", {
@@ -19,10 +32,25 @@ const postData = async (payload) => {
   console.log(status);
 };
 
-// postData(post3);
+addBtn.addEventListener("click", () => {
+  console.log("test");
+  const payload = {
+    title: inputElement.value,
+    views: 0,
+  };
+  postData(payload);
+  inputElement.value = "";
+  const liElement = document.createElement("li");
+  liElement.textContent = `${payload.title} ------ views: ${payload.views}`;
+  listElement.appendChild(liElement);
+});
 
 // Izmena podataka preko "fetch" funkcije, uz pomoc PUT metode
-const post3Modified = { id: "3", title: "Modified another title 3", views: 350 }; // mockovan payload
+const post3Modified = {
+  id: "3",
+  title: "Modified another title 3",
+  views: 350,
+}; // mockovan payload
 
 const putData = async (payload) => {
   const response = await fetch(`http://localhost:3000/posts/${payload.id}`, {
@@ -41,8 +69,8 @@ const deleteData = async (id) => {
     method: "DELETE",
   });
   const status = await response.status;
-  console.log('response: ', response);
+  console.log("response: ", response);
   console.log(status);
 };
 
-deleteData(3);
+// deleteData(3);
