c092fd37a72190afaece3df61565f3f8eff89101
Jovan Medarovic <medarovicjovan@gmail.com>
2025-11-28 11:36:05 +0100

    #3

--- script.js
+++ script.js
@@ -1,65 +1,81 @@
 const listElement = document.getElementById("listToShow");
 
 const inputElement = document.getElementById("inputPostName");
-const addBtn = document.getElementById("addPostBtn");
+const addBtnElement = document.getElementById("addPostBtn");
+
+const inputEditElement = document.getElementById("editPostName");
+const editPostBtnElement = document.getElementById("editPostBtn");
 
 // Prikupljanje podataka preko "fetch" funkcije, uz pomoc GET metode
 const getData = async () => {
   const response = await fetch("http://localhost:3000/posts");
   const posts = await response.json();
   posts.map((post) => {
+    const liElement = document.createElement("li");
     const deleteBtnElement = document.createElement("button");
+    deleteBtnElement.addEventListener('click', async () => {
+      await fetch(`http://localhost:3000/posts/${post.id}`,{
+          method:"DELETE",
+      }).then(() => {
+        liElement.remove();
+      }).catch((reason) => {
+        console.error(reason);
+      });
+    });
     deleteBtnElement.textContent = "X";
+
+    const editBtnElement = document.createElement("button");
+    editBtnElement.textContent = "Edit";
+    editBtnElement.disabled = false;
+    editBtnElement.addEventListener('click',() => {
+      editBtnElement.disabled = true;
+      inputEditElement.value = post.title;
+      editPostBtnElement.addEventListener("click", async () => {
+        const payload = {
+          id: post.id,
+          title: inputEditElement.value,
+          views: post.views,
+        }
+        putData(payload).then(() => {
+          listElement.innerHTML=``;
+          getData();
+        });
+      });
+    });
+    
     const pElement = document.createElement("p");
     pElement.textContent=`${post.title} ------ views: ${post.views}`;
-    const liElement = document.createElement("li");
+    
     liElement.appendChild(pElement);
     liElement.appendChild(deleteBtnElement);
+    liElement.appendChild(editBtnElement);
     listElement.appendChild(liElement);
   });
 };
-getData();
+
 
 // postovanje podataka preko "fetch" funkcije, uz pomoc POST metode
 // const post3 = { id: "3", title: "another title 3", views: 300 }; // mockovan payload
 
 const postData = async (payload) => {
-  const response = await fetch("http://localhost:3000/posts", {
+  await fetch("http://localhost:3000/posts", {
     method: "POST",
     body: JSON.stringify(payload),
-  });
-  const status = await response.status;
-  console.log(status);
+  }).then(response => {console.log(response.json())});
 };
 
-addBtn.addEventListener("click", () => {
-  console.log("test");
-  const payload = {
-    title: inputElement.value,
-    views: 0,
-  };
-  postData(payload);
-  inputElement.value = "";
-  const liElement = document.createElement("li");
-  liElement.textContent = `${payload.title} ------ views: ${payload.views}`;
-  listElement.appendChild(liElement);
-});
-
 // Izmena podataka preko "fetch" funkcije, uz pomoc PUT metode
-const post3Modified = {
-  id: "3",
-  title: "Modified another title 3",
-  views: 350,
-}; // mockovan payload
+// const post3Modified = {
+//   id: "3",
+//   title: "Modified another title 3",
+//   views: 350,
+// }; // mockovan payload
 
-const putData = async (payload) => {
-  const response = await fetch(`http://localhost:3000/posts/${payload.id}`, {
+const putData = async (payload) =>
+  await fetch(`http://localhost:3000/posts/${payload.id}`, {
     method: "PUT",
     body: JSON.stringify(payload),
   });
-  const status = await response.status;
-  console.log(status);
-};
 
 // putData(post3Modified);
 
@@ -73,4 +89,17 @@ const deleteData = async (id) => {
   console.log(status);
 };
 
-// deleteData(3);
+getData();
+
+addBtnElement.addEventListener("click", () => {
+  console.log("test");
+  const payload = {
+    title: inputElement.value,
+    views: 0,
+  };
+  postData(payload);
+  inputElement.value = "";
+  const liElement = document.createElement("li");
+  liElement.textContent = `${payload.title} ------ views: ${payload.views}`;
+  listElement.appendChild(liElement);
+});
